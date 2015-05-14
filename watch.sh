301a8ab40e8d398059ed74d42972b8144ca6f787
Noah Yarian <noah.yarian@gmail.com>
2015-05-14 14:28:35 -0500

    most of the initial setup

---
+++ watch.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# clean and prepare public directory
+rm -rf public
+cp -r src public
+
+# compile jade to html
+./node_modules/.bin/nodemon -e jade --watch src --exec "
+  ./node_modules/.bin/jade src -P
+  cd src
+  find . -name '*.html' | cpio -pdvm ../public
+  cd ..
+  echo 'test'
+  rm -rf src/*.html \
+         src/**/*.html \
+         public/_*.html \
+         public/**/_*.html \
+         public/*.jade \
+         public/**/*.jade \
+         public/_partials" &
+
+# compile sass to css
+./node_modules/.bin/node-sass \
+  --output-style compressed \
+  --source-map-embed \
+  --recursive \
+  src/_styles/main.scss public/css/main.css
+
+# convert ES6 JS to ES5
+./node_modules/.bin/babel \
+  src \
+  --out-dir public \
+  -s inline \
+  -w &
+
+# concat bower_components to public/lib directory
+if [ -d "bower_components" ]; then
+  ./node_modules/.bin/bowcat . -o public/lib -m
+fi
+
+# clean unneeded files
+rm -rf public/_styles \
+       public/*.jade \
+       public/**/*.jade \
+       public/*.scss \
+       public/**/*.scss
